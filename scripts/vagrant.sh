35952f399f37399c4fa782c0f8f9851979f47062
Felix Imobersteg <felix@whatwedo.ch>
2015-07-31 00:29:50 +0200

    perf(vagrant): install nfs-common and portmap on build instead of the first boot

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -2,11 +2,16 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
+#Set build time
 date > /etc/vagrant_box_build_time
 
+#Set vagrant keys
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate \
     'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
+
+#Install sync tools
+apt-get install -y nfs-common portmap
