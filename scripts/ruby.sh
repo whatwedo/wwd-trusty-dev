ebd6ffe6b0379fd4931eebfe57af695c650ee05d
Felix Imobersteg <felix@whatwedo.ch>
2015-08-03 18:55:06 +0200

    fix(rvm): fix rvm permissions

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -9,6 +9,9 @@ apt-get install -y ruby ruby-dev
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
 
+#Set rvm permissions
+chmod -R 777 /usr/local/rvm
+
 #Install mailcatcher
 gem install mailcatcher
 
